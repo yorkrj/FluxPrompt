7b16b770e431d935ac9fc8dcffef0ab31d98fb3d
yorkrj <31115331+yorkrj@users.noreply.github.com>
2021-02-27 11:28:07 -0500

    Intital UI layout and behavior.

--- FluxPrompt/Form1.cs
+++ FluxPrompt/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +14,163 @@ namespace FluxPrompt
 {
     public partial class Form1 : Form
     {
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        HotKeyHandler hotkeyHandler;
+
         public Form1()
         {
             InitializeComponent();
+
+            ShowInTaskbar = false;
+            notifyIcon1.Icon = SystemIcons.Asterisk;
+            TopMost = true;
+
+            TransparencyKey = (BackColor);
+            notifyIcon1.Visible = true;
+            notifyIcon1.Text = "Flux Prompt";
+
+            hotkeyHandler = new HotKeyHandler();
+
+            dataGridView1.Columns.Add(new DataGridViewColumn());
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[0].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.RowHeadersVisible = false;
+            //dataGridView1.AllowUserToAddRows = false;
+
+            hotkeyHandler.HotKeyPressed += new EventHandler<HotKeyPressedEventArgs>(HandleHotKeyPressed);
+
+            hotkeyHandler.Register(0,
+                new HotKeyModifer[] { HotKeyModifer.Alt, HotKeyModifer.NoRepeat },
+                Keys.Space.GetHashCode());
         }
 
+        public void HandleHotKeyPressed(object sender, HotKeyPressedEventArgs e)
+        {
+            //TODO switch for multiple hot key indexes here based on value of e.HotKeyIndex
+
+            WindowState = FormWindowState.Normal;
+            TopMost = true;
+            Activate();
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            //e.Modifiers
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    string csScript = PromptTextBox.Text.ToString();
+
+                    object result = new object();
+
+                    try
+                    {
+                        CSharpScript.EvaluateAsync(csScript).ContinueWith(s => result = s.Result).Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        result = ex.Message;
+                    }
+
+                    dataGridView1.Rows.Insert(0, csScript + Environment.NewLine + Convert.ToString(result));
+
+                    e.Handled = true;
+
+                    break;
+                case Keys.Escape:
+                    WindowState = FormWindowState.Minimized;
+                    break;
+                case Keys.Tab:
+                    // TODO autocomplete
+                    break;
+                default:
+                    //dataGridView1.Rows.Add(TextBox1.Text.ToString());
+                    break;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+            TopMost = true;
+            Activate();
+        }
+
+        private void OnResize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                TopMost = false;
+            }
+            else
+            {
+                TopMost = true;
+            }
+        }
+
+        private void OnFormClosing(object sender, FormClosingEventArgs e)
+        {
+            hotkeyHandler.Close();
+            notifyIcon1.Visible = false;
+        }
+
+        private void TextBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void OnActivated(object sender, EventArgs e)
+        {
+            Opacity = 1;
+        }
+
+        private void OnDeactivate(object sender, EventArgs e)
+        {
+            Opacity = 0.70;
+        }
+
+        private void OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            //char what = e.KeyChar;
+
+            //switch (e.KeyChar)
+            //{
+            //    case '\r':
+            //        e.Handled = true;
+            //        break;
+            //}
+        }
+
+        /// <summary>
+        /// Handle certain key presses to prevent audible alerts and unwanted movement of cursor.
+        /// </summary>
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Up:
+                case Keys.Down:
+                case Keys.Tab:
+                case Keys.Enter:
+                case Keys.Escape:
+                    e.Handled = true;
+                    e.SuppressKeyPress = true;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
---
+++ FluxPrompt/HotKeyHandler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FluxPrompt
+{
+    /// <summary>
+    /// HotKeyHandler allows for the registration and handling of global hot keys even when the application has been minimized to the notification tray.
+    /// </summary>
+    class HotKeyHandler : NativeWindow
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr windowHandle, int id);
+
+        public const int WM_HOTKEY = 0x0312;
+
+        public event EventHandler<HotKeyPressedEventArgs> HotKeyPressed;
+
+        public HotKeyHandler()
+        {
+            this.CreateHandle(new CreateParams());
+
+            // TODO: Make this default hotkey configurable.
+            Register(0,
+                new HotKeyModifer[] { HotKeyModifer.Alt, HotKeyModifer.NoRepeat },
+                Keys.Space.GetHashCode());
+        }
+
+        public bool Register(int id, HotKeyModifer[] modiferKeys, int virtualKeyCode)
+        {
+            int fsModifiers = 0;
+
+            foreach (HotKeyModifer modiferKey in modiferKeys)
+            {
+                fsModifiers |= (int)modiferKey;
+            }
+
+            return RegisterHotKey(Handle, id, fsModifiers, virtualKeyCode);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_HOTKEY)
+            {
+                int hotkeyID = m.WParam.ToInt32();
+
+                HotKeyPressed?.Invoke(this, new HotKeyPressedEventArgs(hotkeyID));
+            }
+            base.WndProc(ref m);
+        }
+
+        public bool Unregister(int id)
+        {
+            return UnregisterHotKey(Handle, id);
+        }
+
+        /// <summary>
+        /// Clean up registered hot keys. Object will be unusable after this call.
+        /// </summary>
+        public void Close()
+        {
+            UnregisterHotKey(Handle, 0); // TODO keep track of hot keys registered and unregister all here. For now we just have the one default.
+            DestroyHandle();
+        }
+    }
+
+    public enum HotKeyModifer
+    {
+        Alt = 0x0001,
+        Control = 0x0002,
+        Shift = 0x0004,
+        Win = 0x0008,
+        NoRepeat = 0x4000
+    }
+
+    public class HotKeyPressedEventArgs: EventArgs
+    {
+        private int hotKeyIndex;
+
+        public int HotKeyIndex 
+        {
+            get { return hotKeyIndex; } 
+        }
+
+        public HotKeyPressedEventArgs(int hotKeyIndex)
+        {
+            this.hotKeyIndex = hotKeyIndex;
+        }
+    }
+}
